691a37d3eb679803ba5d4c8c8bac8f279da34366
e.b.olson <e.b.olson.mobile@gmail.com>
2023-07-24 14:31:01 -0700

    Testing out using a manual bridge header.

---
+++ Sources/Ono/include/Ono-Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Header.h
+//  
+//
+//  Created by e.b.olson on 7/19/23.
+//
+
+#ifndef OnoPackage_h
+#define OnoPackage_h
+
+#include "Ono.h"
+#include "ONOXMLDocument.h"
+
+#endif /* Header_h */
